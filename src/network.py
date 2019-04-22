392e8827645562c401cee7529b7bd36542356a56
Alex Robey <arobey1@huntsman-ve504-0988.apn.wlan.upenn.edu>
2019-04-22 14:54:25 -0400

    added setter so that rect isn't passed directly to Network

--- src/network.py
+++ src/network.py
@@ -1,9 +1,12 @@
 import numpy as np
+import os
+from scipy.io import savemat
 
 
 class Network:
 
-    def __init__(self, dim_list, activation, rect, weight_type):
+    def __init__(self, dim_list, activation, weight_type, fix_weights=False,
+                    load_weights=False, weight_root=None):
         """Constructor for neural network class
         params:
             dim_list: list of ints      - dimensions of each layer in network
@@ -11,20 +14,34 @@ class Network:
                                         - 'relu' | 'sigmoid' | 'tanh'
             rect: instance of Rectangle - upper and lower bounds on input
             weight_type: str            - 'ones' | 'rand'
+            fix_weights: bool           - fix random seed for weights if True
         """
 
         # activation function for network
         self._activation = activation
 
         # dimensions of layers
-        self._dim_input, self._dim_output = dim_list[0], dim_list[2]
-        self._dim_hidden_layer = dim_list[1]
-        self._num_hidden_layers = 1
+        self._dim_list = dim_list
+        self._num_hidden_layers = len(dim_list) - 2
 
         self._weight_type = weight_type
-        self._rect = rect
+        self._fix_weights = fix_weights
+        self._rect = None
 
-        self._create_weights()
+        if load_weights == False:
+            self._create_weights()
+        else:
+            self._weight_root = weight_root
+            self._load_weights()
+
+
+    @property
+    def rect(self):
+        return self._rect
+
+    @rect.setter
+    def rect(self, instance):
+        self._rect = instance
         self._calc_lower_and_upper_bounds()
 
     def _create_weights(self):
@@ -36,30 +53,42 @@ class Network:
         self._W, self._b = {}, {}
 
         if self._weight_type == 'rand':
-            # np.random.seed(5)
-            self._W[0] = c * np.random.randn(self._dim_hidden_layer, self._dim_input)
-            self._b[0] = c * np.random.randn(self._dim_hidden_layer, 1)
 
-            self._W[1] = c * np.random.randn(self._dim_output, self._dim_hidden_layer)
-            self._b[1] = c * np.random.randn(self._dim_output, 1)
+            if self._fix_weights:
+                np.random.seed(5)
+
+            for i in range(self._num_hidden_layers + 1):
+                self._W[i] = c * np.random.randn(self._dim_list[i+1], self._dim_list[i])
+                self._b[i] = c * np.random.randn(self._dim_list[i+1], 1)
 
         elif self._weight_type == 'ones':
-            self._W[0] = c * np.ones(shape=(self._dim_hidden_layer, self._dim_input))
-            self._b[0] = c * np.ones(shape=(self._dim_hidden_layer, 1))
 
-            self._W[1] = c * np.ones(shape=(self._dim_output, self._dim_hidden_layer))
-            self._b[1] = c * np.ones(shape=(self._dim_output, 1))
+            for i in range(self._num_hidden_layers + 1):
+                self._W[i] = c * np.ones(shape=(self._dim_list[i+1], self._dim_list[i]))
+                self._b[i] = c * np.ones(shape=(self._dim_list[i+1], 1))
 
         else:
             raise ValueError('Please use weight_type = "rand" | "ones"')
 
+    def _load_weights(self):
+
+        self._W, self._b = {}, {}
+
+        weights = np.load(os.path.join(self._weight_root, 'failed_weights100.npy'), allow_pickle=True)
+        biases = np.load(os.path.join(self._weight_root, 'failed_biases100.npy'), allow_pickle=True)
+
+        for i in range(self._num_hidden_layers + 1):
+            self._W[i] = weights[i]
+            self._b[i] = biases[i]
+
+
     def forward_prop(self, X):
         """Compute one forward pass through the network
         params:
-            X: d x n array of floats - training instances
+            X: (d,n) array of floats - training instances
 
         returns:
-            Y: dout x n array of floats - output from training instances"""
+            Y: (dout,n) array of floats - output from training instances"""
 
         # compute for each layer in network
         # TODO: vectorize?
@@ -96,28 +125,39 @@ class Network:
             self._y_lower_bounds[1] = np.maximum(self._x_lower_bounds[1], 0)
             self._y_upper_bounds[1] = np.maximum(self._x_upper_bounds[1], 0)
 
+    def save_params(self, root):
+
+        weights = self._weights_as_list()
+        biases = self._biases_as_list()
+
+        np.save(os.path.join(root, 'failed_weights' + str(self._dim_list[1]) + '.npy'), weights)
+        np.save(os.path.join(root, 'failed_biases' + str(self._dim_list[1]) + '.npy'), biases)
+
+        savemat(os.path.join(root, 'failed_weights' + str(self._dim_list[1]) + '.npy'), mdict={'W': weights})
+        savemat(os.path.join(root, 'failed_biases' + str(self._dim_list[1]) + '.npy'), mdict={'b': biases})
+
     def total_num_hidden_units(self):
         """Getter method for total number of hidden units in network"""
 
-        return self._dim_hidden_layer
+        return sum(self._dim_list[1:len(self._dim_list)-1])
 
     @property
     def dim_input(self):
         """Getter method for input dimension of neural network"""
 
-        return self._dim_input
+        return self._dim_list[0]
 
     @property
     def dim_output(self):
         """Getter method for output dimension of neural network"""
 
-        return self._dim_output
+        return self._dim_list[-1]
 
     @property
     def dim_hidden_layer(self):
         """Getter method for list of dimensions of hidden layers in neural network"""
 
-        return self._dim_hidden_layer
+        return self._dim_list[1]
 
     @property
     def W(self):
@@ -141,21 +181,17 @@ class Network:
 
         return self._x_upper_bounds
 
-    # @lower_bounds.setter
-    # def lower_bounds(self, key, value):
-    #     """Setting method dictionary of lower bounds of neural network"""
-    #
-    #     self._lower_bounds[key] = value
-    #
-    # @upper_bounds.setter
-    # def upper_bounds(self, key, value):
-    #     """Setting method dictionary of upper bounds of neural network"""
-    #
-    #     self._upper_bounds[key] = value
+    def _weights_as_list(self):
+
+        return [w for w in self._W.values()]
+
+    def _biases_as_list(self):
+
+        return [b for b in self._b.values()]
 
 
 if __name__ == '__main__':
-    dim_list = [2, 20, 50, 2]
+    dim_list = [10, 20, 50, 2]
     activation = 'relu'
-    n = Network(dim_list, activation)
-    print(n.dim_hidden[-1])
+    net = Network(dim_list, activation, 1, 'rand')
+    print([w.shape for w in net.W.values()])
